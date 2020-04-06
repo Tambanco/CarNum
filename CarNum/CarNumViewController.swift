30e414857778a29c8f4098984aa14961570e2a1d
Tambanco <a.tambanco@gmail.com>
2020-04-06 12:45:19 +0300

    add "ADD" button into navigation bar

--- CarNum/CarNumViewController.swift
+++ CarNum/CarNumViewController.swift
@@ -9,23 +9,40 @@
 import UIKit
 
 class CarNumViewController: UITableViewController {
-
-    let itemArray = ["aa999a777", "oo888o99", "ва757а54"]
+    
+    let itemArray = ["aa999a777", "oo888o99", "ва564а54", "aa432a777", "oo888o99", "ва757а54", "aa999a777", "oo888o99", "ва757а54", "aa999a777", "oo888o99", "ва757а54", "aa999a777", "oo888o99", "ва757а54", "aa999a777", "oo888o99", "ва564а54", "aa432a777", "oo888o99", "ва757а54", "aa999a777", "oo888o99", "ва757а54", "aa999a777", "oo888o99", "ва757а54", "aa999a777", "oo888o99", "ва757а54"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
     }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CarNumCell", for: indexPath)
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CarNumCell", for: indexPath)
+        
         cell.textLabel?.text = itemArray[indexPath.row]
         
         return cell
+        
     }
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("section: \(indexPath.section)")
+//        print("row: \(indexPath.row)")
+//        print("\(itemArray[indexPath.row])")
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
 }
+
