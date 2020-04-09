44981ab6f6d135714c38066ba7d6cf95bb53c5cf
Tambanco <a.tambanco@gmail.com>
2020-04-09 22:15:24 +0300

    added categoryViewController

---
+++ CarNum/Controllers/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  CarNum
+//
+//  Created by Tambanco on 09.04.2020.
+//  Copyright © 2020 Tambanco. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addCategoryPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
