1181a13776dcdd5ace7fd95c669ca0954db64bb4
Al <alexunder_5542@mail.ru>
2021-05-23 10:03:53 +0300

    added testTableViewNotNilWhenViewIsLoaded for CurNumViewContoller

---
+++ CarNumTests/Controller/CarNumViewControllerTests.swift
@@ -0,0 +1,31 @@
+//
+//  CarNumViewControllerTests.swift
+//  CarNumTests
+//
+//  Created by tambanco 🥳 on 23.05.2021.
+//  Copyright © 2021 Tambanco. All rights reserved.
+//
+
+import XCTest
+@testable import CarNum
+
+class CarNumViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testTableViewNotNilWhenViewIsLoaded() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: String(describing: CarNumViewController.self))
+        let sut = vc as! CarNumViewController
+        sut.loadViewIfNeeded()
+        
+        XCTAssertNotNil(sut.tableView)
+    }
+ 
+}
